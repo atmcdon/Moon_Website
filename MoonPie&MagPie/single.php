c5fc2da1d6b8a7574979a81c99f3c5a1c189481e
Andrew McDonald <68485649+atmcdon@users.noreply.github.com>
2024-02-29 10:00:24 -0800

    Delete MoonPie&MagPie directory

--- MoonPie&MagPie/single.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<div class="container">
-
-    <?php if(have_posts() ) : while(have_posts()) : the_post();?>
-    <?php the_content();?>
-    <?php endwhile; else: endif;?>
-
-</div>
